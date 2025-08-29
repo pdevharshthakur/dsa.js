8b2cb351731f373e7f4f000efa0b96b232d330fd
Harsh Thakur <205749908+pdevharshthakur@users.noreply.github.com>
2025-08-29 17:11:57 +0530

    feat: selection sort

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -1,4 +1,21 @@
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      // Swap them minIndex and i in arr
+      const temp = arr[minIndex];
+      arr[minIndex] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
+}
 
 function main() {
   const arr = [10, 5, 12, 1, 3]; // An example array to sort
